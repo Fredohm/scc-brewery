bd8c2db8e6d94c0bf7974a8e63b2e998b39198e4
Fredohm <fredwaucquez@hotmail.fr>
2020-12-24 09:04:35 +0100

    25. HTTP Method Matching

--- src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
@@ -14,12 +14,12 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
-public class BaseIT {
+public abstract class BaseIT {
 
     @Autowired
     WebApplicationContext wac;
 
-    MockMvc mockMvc;
+    protected MockMvc mockMvc;
 
     @MockBean
     BeerRepository beerRepository;
---
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -0,0 +1,24 @@
+package guru.sfg.brewery.web.controllers.api;
+
+import guru.sfg.brewery.web.controllers.BaseIT;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class BeerRestControllerIT extends BaseIT {
+
+    @Test
+    void findBeers() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/5deb7d1f-adbc-4cf0-85ea-5d1e5d6f0486"))
+                .andExpect(status().isOk());
+    }
+}
