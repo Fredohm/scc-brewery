1e195c3d44ba6c5f0c869a225ff99ed6939407e5
Fredohm <fredwaucquez@hotmail.fr>
2020-12-24 09:15:00 +0100

    26. Spring MVC Path Matchers

--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -21,4 +21,10 @@ public class BeerRestControllerIT extends BaseIT {
         mockMvc.perform(get("/api/v1/beer/5deb7d1f-adbc-4cf0-85ea-5d1e5d6f0486"))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    void findBeerByUpc() throws Exception {
+        mockMvc.perform(get("/api/v1/beerUpc/*"))
+                .andExpect(status().isOk());
+    }
 }
