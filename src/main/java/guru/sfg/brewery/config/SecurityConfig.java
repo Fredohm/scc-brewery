245beaaf2c40c2b50c3aeb9833bd9f1f1b661339
Fredohm <fredwaucquez@hotmail.fr>
2020-12-24 11:30:16 +0100

    30b. Added a user.

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -37,7 +37,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .withUser("user")
                     .password("{noop}password")
-                    .roles("USER");
+                    .roles("USER")
+                .and()
+                    .withUser("scott")
+                    .password("{noop}tiger")
+                    .roles("CUSTOMER");
     }
 
 //    @Override
