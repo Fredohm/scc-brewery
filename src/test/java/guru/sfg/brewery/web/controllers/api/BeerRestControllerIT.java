8be92e7a59a770cb11e42edea5b6d0a3b34388f1
Fredohm <fredwaucquez@hotmail.fr>
2020-12-25 11:35:46 +0100

    46. Custom Do Filter Method

--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -1,10 +1,10 @@
 package guru.sfg.brewery.web.controllers.api;
 
 import guru.sfg.brewery.web.controllers.BaseIT;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,11 +30,23 @@ public class BeerRestControllerIT extends BaseIT {
                 .andExpect(status().isOk());
     }
 
-    @Disabled
     @Test
     void deleteBeer() throws Exception{
-        mockMvc.perform(delete("/api/v1/beer5deb7d1f-adbc-4cf0-85ea-5d1e5d6f0486/")
-            .header("Api-Key", "spring").header("Api-Secret", "guru"))
+        mockMvc.perform(delete("/api/v1/beer/5deb7d1f-adbc-4cf0-85ea-5d1e5d6f0486/")
+                .header("Api-Key", "spring").header("Api-Secret", "guru"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteBeerHttpBasic()  throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/5deb7d1f-adbc-4cf0-85ea-5d1e5d6f0486/")
+                .with(httpBasic("spring", "guru")))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void deleteBeerNoAuth() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/5deb7d1f-adbc-4cf0-85ea-5d1e5d6f0486"))
+                .andExpect(status().isUnauthorized());
     }
 }
