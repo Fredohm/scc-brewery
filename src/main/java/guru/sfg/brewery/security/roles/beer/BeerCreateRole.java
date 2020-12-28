d469fbfd9fe0d9ecb9449c5da3fe730e4505298e
Fredohm <fredwaucquez@hotmail.fr>
2020-12-28 08:38:13 +0100

    70. Custom Authorization Annotations

---
+++ src/main/java/guru/sfg/brewery/security/roles/beer/BeerCreateRole.java
@@ -0,0 +1,12 @@
+package guru.sfg.brewery.security.roles.beer;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAuthority('beer.create')")
+public @interface BeerCreateRole {
+
+}
