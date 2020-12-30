e6dedf639da26cd4e98131c1f6b04486b6554148
Fredohm <fredwaucquez@hotmail.fr>
2020-12-30 12:02:57 +0100

    81. Using Authentication Principle

---
+++ src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderControllerV2.java
@@ -0,0 +1,55 @@
+package guru.sfg.brewery.web.controllers.api;
+
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.security.roles.order.BeerOrderReadRoleV2;
+import guru.sfg.brewery.services.BeerOrderService;
+import guru.sfg.brewery.web.model.BeerOrderDto;
+import guru.sfg.brewery.web.model.BeerOrderPagedList;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v2/orders/")
+//@RequiredArgsConstructor
+@RestController
+public class BeerOrderControllerV2 {
+
+    public static final Integer DEFAULT_PAGE_NUMBER = 0;
+    public static final Integer DEFAULT_PAGE_SIZE = 25;
+
+    private final BeerOrderService beerOrderService;
+
+    public BeerOrderControllerV2(BeerOrderService beerOrderService) {
+        this.beerOrderService = beerOrderService;
+    }
+
+    @BeerOrderReadRoleV2
+    @GetMapping
+    public BeerOrderPagedList listOrders(@AuthenticationPrincipal User user,
+                                         @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+                                         @RequestParam(value = "pageSize", required = false) Integer pageSize) {
+
+        if (pageNumber == null || pageNumber < 0) {
+            pageNumber = DEFAULT_PAGE_NUMBER;
+        }
+
+        if (pageSize == null || pageSize < 1) {
+            pageSize = DEFAULT_PAGE_SIZE;
+        }
+
+        if (user.getCustomer() != null) {
+            return beerOrderService.listOrders(user.getCustomer().getId(), PageRequest.of(pageNumber, pageSize));
+        } else {
+            return beerOrderService.listOrders(PageRequest.of(pageNumber, pageSize));
+        }
+    }
+
+    @BeerOrderReadRoleV2
+    @GetMapping("orders/{orderId}")
+    public BeerOrderDto getOrder(@PathVariable("orderId") UUID orderId) {
+        return null;
+        // return beerOrderService.getOrderById(orderId);
+    }
+}
