ab9dee4abf5c9ba07bb1a86410fa266b44594adf
Fredohm <fredwaucquez@hotmail.fr>
2020-12-31 08:42:19 +0100

    97. Persistent Token Rememeber Me  - IndexControllerTest -> build failure  replace with @SpringBootTest

--- src/test/java/guru/sfg/brewery/web/controllers/IndexControllerTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/IndexControllerTest.java
@@ -7,13 +7,13 @@ import guru.sfg.brewery.services.BeerOrderService;
 import guru.sfg.brewery.services.BeerService;
 import guru.sfg.brewery.services.BreweryService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@SpringBootTest
 public class IndexControllerTest extends BaseIT {
 
     @MockBean
