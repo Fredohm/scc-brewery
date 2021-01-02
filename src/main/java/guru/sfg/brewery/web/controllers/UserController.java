f34d2d30aa5014ebf25a709bb7bb5010e579220d
Fredohm <fredwaucquez@hotmail.fr>
2021-01-02 06:55:06 +0100

    119. Java Configuration and 2FA Demo

--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -38,7 +38,7 @@ public class UserController {
         return "user/register2fa";
     }
 
-    @PostMapping
+    @PostMapping("/register2fa")
     public String confirm2fa(@RequestParam Integer verifyCode) {
 
         User user = getUser();
@@ -62,7 +62,7 @@ public class UserController {
         return "user/verify2fa";
     }
 
-    @PostMapping
+    @PostMapping("/verify2fa")
     public String verifyPostOf2fa(@RequestParam Integer verifyCode) {
 
         User user = getUser();
@@ -71,7 +71,7 @@ public class UserController {
 
             ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).setGoogle2faRequired(false);
 
-            return "/index";
+            return "index";
         } else {
             return "user/verify2fa";
         }
